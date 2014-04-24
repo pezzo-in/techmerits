72450a6ab564d9a834d9800f6b7dcc64543b545c
Harikrishna <hari.krishna@pezzo.in>
2014-04-24 13:11:39 +0530

    small changes done in css.

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 	
 		
 		<div id="copyright">
-			<center class="footerfont">&copy; 2014 - All Rights Reserved - Techmerits Software Solutions.</center>
+			<center>&copy; 2014 - All Rights Reserved - Techmerits Software Solutions.</center>
 
 			<br class="clear"/>
 		</div>
