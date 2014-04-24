145ed0fd3ffdfa4ce829f507c4b61dad65e367b0
Harikrishna <hari.krishna@pezzo.in>
2014-04-24 12:17:20 +0530

    footer.php changed for email sending purpose.

--- footer.php
+++ footer.php
@@ -1,11 +1,9 @@
-<div class="wrapper col5 footer-master">
-	<div id="footer">
-		<!-- End Company Details -->
+
+	
+		
 		<div id="copyright">
 			<center class="footerfont">&copy; 2014 - All Rights Reserved - Techmerits Software Solutions.</center>
 
 			<br class="clear"/>
 		</div>
 		<div class="clear"></div>
-	</div>
-</div>
