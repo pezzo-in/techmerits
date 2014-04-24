129f8e44a34836118c0f5f5fd86163a28fae77ba
pavan <pavan@graphicana.co.in>
2014-04-24 18:47:13 +0530

    corrections

--- contactus.php
+++ contactus.php
@@ -33,11 +33,11 @@
             </li>
         </ul>
     </div>
-    <div id="gallery">
-        <img src="images/demo/services.jpg" alt=""/>
+</div>
+<div id="gallery">
+    <img src="images/demo/services.jpg" alt=""/>
 
-        <div class="clear"></div>
-    </div>
+    <div class="clear"></div>
 </div>
 <div class="wrapper col4">
     <div id="container">
