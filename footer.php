352e1df681a251adad5068e06a66ee65a3b2f0b2
pavan <pavan@graphicana.co.in>
2014-05-01 20:10:10 +0530

    footer changes

--- footer.php
+++ footer.php
@@ -1,60 +1,61 @@
-<head>
-	<link rel="stylesheet" href="styles/font-awesome.min.css">
-</head>
-	<div id="container" style="background-color:#222222; width:100%;">
-		<div id="address" style="">
-			<div style="font-weight:bold; font-size:16px;">Global Development Centre</div>
+<link rel="stylesheet" href="styles/font-awesome.min.css">
+<div id="footer">
+    <div class="wrapper" style="background: #333333">
+        <div id="social-media-icons" class="footer-content">
+            <div class="social-links">
+                <ul>
+                    <li><a href="#"><i class="fa fa-facebook-square fa-3x"></i></a></li>
+                    <li><a href="#"><i class="fa fa-youtube fa-3x"></i></a></li>
+                    <li><a href="#"><i class="fa fa-google-plus-square fa-3x"></i></a></li>
+                    <li><a href="#"><i class="fa fa-twitter fa-3x"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="wrapper" style="background: #000000">
+        <div class="footer-content">
+            <div class="address"><h3>Global Development Centre</h3>
 
-			<div>
-				Techmerits Software Solutions
-			</div>
-
-			<div>
-				Regus Business centre, 4th Floor,
-			</div>
-			<div>
-				1-10-39 to 44, Gumidelli Towers,
-			</div>
-			<div>
-				Old Airport road, Begumpet
-			</div>
-			<div>
-				Hyderabad - 500016
-			</div>
-			<div>
-				India
-			</div>
-			<div>
-				Email: info@techmerits.com
-			</div>
-			<div>
-				Tel: +91 40 60507879
-			</div>
-		</div>
-		</div>
-		<div id="container" style="background-color:#111111; width:100%;">
-		<div id="social-media-icons">
-			<div class="social-links">
-
-				<ul>
-					<li><a href="#" ><i class="fa fa-facebook-square fa-3x"></i></a></li>
-					<li><a href="#" ><i class="fa fa-youtube fa-3x"></i></a></li>
-					<li><a href="#" ><i class="fa fa-google-plus-square fa-3x"></i></a></li>
-					<li><a href="#" ><i class="fa fa-twitter fa-3x"></i></a></li>
-
-				</ul>
-
-			</div>
-
-		</div>
-		</div>
-		<div id="container" style="background-color:#222222; width:100%;">
-		<div id="footer-content">
-		<div style="float:left">
-		<a href="sitemap.php">SiteMap</a>
-		</div>
-		<div style="float:right;">
-			&copy; 2014 - All Rights Reserved - Techmerits Software Solutions.
-			</div>
-		</div>
-		</div>
+                <p>Techmerits Software Solutions <br/>
+                    Regus Business centre, 4th Floor, <br/>
+                    1-10-39 to 44, Gumidelli Towers, <br/>
+                    Old Airport road, Begumpet <br/>
+                    Hyderabad - 500016 <br/>
+                    India <br/>
+                    Email: info@techmerits.com <br/>
+                    Tel: +91 40 60507879</p>
+            </div>
+            <ul class="nav">
+                <li>
+                    <a href="index.php"><strong>HOME</strong></a>
+                </li>
+                <li>
+                    <ul>
+                        <li><strong>COMPANY</strong></li>
+                        <li><a href="overview.php">Overview</a></li>
+                        <li><a href="management.php">Management</a></li>
+                        <li><a href="contactus.php">Offices</a></li>
+                        <li><a href="careers.php">Careers</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <ul>
+                        <li><strong>SERVICES</strong></li>
+                        <li><a href="software-development-services.php">Development Services</a></li>
+                        <li><a href="testing-services.php">Testing Services</a></li>
+                        <li><a href="project-management.php">Project Management</a></li>
+                        <li><a href="consulting.php">IT Consulting</a></li>
+                    </ul>
+                </li>
+                <li><a href="contactus.php"><strong>CONTACT US</strong></a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="wrapper" style="background: #333333">
+        <div class="footer-content">
+            <div class="copy-rights">
+                &copy; 2014 - All Rights Reserved - Techmerits Software Solutions.
+            </div>
+        </div>
+    </div>
+</div>
--- header.php
+++ header.php
@@ -5,51 +5,24 @@
                 <li>
                     <a href="index.php">HOME</a>
                 </li>
-                <li class="non-active">
-                COMPANY
-
+                <li class="non-active">COMPANY
                     <ul>
-                     
-                    <li>
-                            <a href="overview.php">Overview</a>
-                        </li>
-
-                        <li>
-                            <a href="management.php">Management</a>
-                        </li>
+                        <li><a href="overview.php">Overview</a></li>
+                        <li><a href="management.php">Management</a></li>
+                        <li><a href="contactus.php">Offices</a></li>
+                        <li><a href="careers.php">Careers</a></li>
+                    </ul>
                 </li>
-                <li>
-                    <a href="contactus.php">Offices</a>
+                <li class="logo"><a href="index.php"><img src="images/demo/logo-bluebg.png" width="230"></a></li>
+                <li class="non-active">SERVICES
+                    <ul>
+                        <li><a href="software-development-services.php">Development Services</a></li>
+                        <li><a href="testing-services.php">Testing Services</a></li>
+                        <li><a href="project-management.php">Project Management</a></li>
+                        <li><a href="consulting.php">IT Consulting</a></li>
+                    </ul>
                 </li>
-                <li>
-                    <a href="careers.php">Careers</a>
-                </li>
-            </ul>
-            </li>
-            <li class="logo">
-                <a href="index.php"><img src="images/demo/logo-bluebg.png" width="230"></a>
-            </li>
-            <li  class="non-active">
-                SERVICES
-                <ul>
-                    <li>
-                        <a href="software-development-services.php">Development Services</a>
-                    </li>
-                    <li>
-                        <a href="testing-services.php">Testing Services</a>
-                    </li>
-                    <li>
-                        <a href="project-management.php">Project Management</a>
-                    </li>
-                    <li>
-                        <a href="consulting.php">IT Consulting</a>
-                    </li>
-
-                </ul>
-            </li>
-            <li>
-                <a href="contactus.php">CONTACT US</a>
-            </li>
+                <li><a href="contactus.php">CONTACT US</a></li>
             </ul>
         </div>
     </div>
