43bab55b2c2a39124133eb8159be3913e9f8aaf4
Harikrishna <hari.krishna@pezzo.in>
2014-05-01 15:49:18 +0530

    footer is added with additional content.

--- footer.php
+++ footer.php
@@ -1,6 +1,53 @@
+<head>
+	<link rel="stylesheet" href="styles/font-awesome.min.css">
+</head>
 <div id="copyright">
-    <center>&copy; 2014 - All Rights Reserved - Techmerits Software Solutions.</center>
+	<div id="container">
+		<div id="address">
+			<div style="font-weight:bold; font-size:16px;">Global Development Centre</div>
 
-    <br class="clear"/>
+			<div>
+				Techmerits Software Solutions
+			</div>
+
+			<div>
+				Regus Business centre, 4th Floor,
+			</div>
+			<div>
+				1-10-39 to 44, Gumidelli Towers,
+			</div>
+			<div>
+				Old Airport road, Begumpet
+			</div>
+			<div>
+				Hyderabad - 500016
+			</div>
+			<div>
+				India
+			</div>
+			<div>
+				Email: info@techmerits.com
+			</div>
+			<div>
+				Tel: +91 40 60507879
+			</div>
+		</div>
+		<div id="social-media-icons">
+			<div class="social-links">
+
+				<ul>
+					<li><a href="#" ><i class="fa fa-facebook-square fa-3x"></i></a></li>
+					<li><a href="#" ><i class="fa fa-youtube fa-3x"></i></a></li>
+					<li><a href="#" ><i class="fa fa-google-plus-square fa-3x"></i></a></li>
+					<li><a href="#" ><i class="fa fa-twitter fa-3x"></i></a></li>
+
+				</ul>
+
+			</div>
+
+		</div>
+		<div id="footer-content">
+			&copy; 2014 - All Rights Reserved - Techmerits Software Solutions.
+		</div>
+	</div>
 </div>
-<div class="clear"></div>
